d62c828247b1d8f7756024d49bdc3e21211cfc0b
nakajimajunichi <nacj4040.magicana@gmail.com>
2023-05-21 16:38:33 +0900

    Created

---
+++ web/bbs.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>paiza掲示板</title>
+</head>
+
+<body>
+    <?php
+
+    ?>
+    <h1>paiza掲示板</h1>
+    <h2>投稿フォーム</h2>
+    <form action="bbs.php" method="post">
+        <label>投稿内容</label>
+        <input type="text" name="content">
+        <button type="submit">送信</button>
+    </form>
+    <h3>発言リスト</h3>
+    <?php
+
+    ?>
+    <table>
+        <tr>
+
+        </tr>
+        <?php
+
+        ?>
+    </table>
+</body>
+
+</html>
