620de3f1f93bda33a484a4c109c2f7301bc6b673
nakajimajunichi <nacj4040.magicana@gmail.com>
2023-05-21 16:30:36 +0900

    Updated

--- skill_up/c055.py
+++ skill_up/c055.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 def list2str(l: list) -> str:
     return "".join(list(map(str, l)))
 
@@ -12,3 +13,18 @@ def line2numlist(line: str) -> list:
 
 def del_duplication(l: list) -> list:
     return list(set(l))
+
+
+n = 5
+g = "ai"
+lst = ["pizza", "paiza", "aizu", "ai", "sai"]
+
+ans = []
+for i in lst:
+    if g in i:
+        ans.append(i)
+if len(ans) > 0:
+    for i in ans:
+        print(i)
+else:
+    print("None")
