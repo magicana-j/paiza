b832da7a22719545017411dc32197a9a1daf6dfb
magicana-j <zima0524@hotmail.co.jp>
2023-06-03 08:00:57 +0900

    Modified

--- competitivecoding.py
+++ competitivecoding.py
@@ -10,8 +10,10 @@ def multi_input():
 def line_to_intlist():
     return list(map(int, input().strip().split()))
 
+
 def line_to_strlist():
     return list(map(str, input().strip().split()))
+
 
 def input_to_n_lists(n: int):
     result = []
@@ -19,8 +21,10 @@ def input_to_n_lists(n: int):
         result.append(line_to_int_list())
     return result
 
+
 def join_list_to_str(l: list):
     return " ".join(list(map(str, l)))
+
 
 def print_each_element(l: list):
     for _ in len(l):
@@ -30,23 +34,26 @@ def print_each_element(l: list):
 """
 int_input()             数値を一つ入力
 multi_int_input()       数値を複数入力
-line_to_int_list()      一行のスペース区切りデータを一次元リストに格納
+line_to_intlist()       一行のスペース区切り整数データを一次元リストに格納
+line_to_strlist()       一行のスペース区切りデータを一次元リストに格納
 input_to_n_lists(n)     n行のスペース区切りデータを多次元リストに格納
 join_list_to_str(l)     一次元数値リストlをスペース区切り文字列に変換
 """
 import fileinput
 
-if __name__=="__main__":
+if __name__ == "__main__":
     n = int(input())
     a1 = []
     b1 = []
     for line in fileinput.input():
         tokens = line.strip.split()
-        a1.append([tokens[0])
-        b1.append([tokens[1])
+        a1.append(tokens[0])
+        b1.append(tokens[1])
 
     m = int(input())
+    a2 = []
+    b2 = []
     for line in fileinput.input():
         tokens = line.strip.split()
-        a2.append([tokens[0])
-        b2.append([tokens[1])
+        a2.append(tokens[0])
+        b2.append(tokens[1])
