90fbc3442ac0fbff18f49b6fdd79e555d9fe080f
magicana-j <zima0524@hotmail.co.jp>
2023-06-03 10:02:58 +0900

    Modified

--- paizatemplate.py
+++ paizatemplate.py
@@ -9,11 +9,3 @@ for line in fileinput.input():
     tokens = line.strip.split()
     a1.append(tokens[0])
     b1.append(tokens[1])
-
-n = int(input())
-a2 = []
-b2 = []
-for line in fileinput.input():
-    tokens = line.strip.split()
-    a2.append(tokens[0])
-    b2.append(tokens[1])
